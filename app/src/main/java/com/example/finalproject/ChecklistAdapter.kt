27e129a61546c3df654ba8cb48843490e23b3a84
hemanshu <hemanshub2002@gmail.com>
2023-12-13 16:42:12 -0500

    started checklist

---
+++ app/src/main/java/com/example/finalproject/ChecklistAdapter.kt
@@ -0,0 +1,38 @@
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.finalproject.R
+
+class ChecklistAdapter(private val items: List<String>) :
+    RecyclerView.Adapter<ChecklistAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.checklist_fragment, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items[position]
+        holder.bind(item)
+    }
+
+
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val checkbox: CheckBox = itemView.findViewById(R.id.todoCheckbox)
+        private val textView: TextView = itemView.findViewById(R.id.todoText)
+
+        fun bind(item: String) {
+            textView.text = item
+
+        }
+    }
+}
