6120b6665403d881f382c73718d8124b72248b09
md499 <mdhir@bu.edu>
2024-01-06 10:45:22 -0500

    updated Take Pciture, Sign In and DB Icons

--- app/src/main/java/com/smh/finalproject/MainDisplayActivity.kt
+++ app/src/main/java/com/smh/finalproject/MainDisplayActivity.kt
@@ -8,7 +8,6 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 
 class MainDisplayActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
@@ -24,49 +23,36 @@ class MainDisplayActivity : AppCompatActivity() {
         val editTextPassword = findViewById<EditText>(R.id.editTextPassword)
         val buttonRegister = findViewById<Button>(R.id.submitButton)
 
-        val returnButton = findViewById<Button>(R.id.returnButton)
-
         buttonRegister.setOnClickListener {
             val enteredUsername = editTextUsername.text.toString()
             val enteredPassword = editTextPassword.text.toString()
 
-            // Check if the user already exists with the entered email
             auth.fetchSignInMethodsForEmail(enteredUsername).addOnCompleteListener { fetchTask ->
                 if (fetchTask.isSuccessful) {
                     val signInMethods = fetchTask.result?.signInMethods ?: emptyList<String>()
 
                     if (signInMethods.isNotEmpty()) {
-                        // User already exists with the entered email, show error message
                         Toast.makeText(
                             this,
                             "User with this email already exists. Please use a different email.",
                             Toast.LENGTH_SHORT
                         ).show()
                     } else {
-                        // User does not exist, proceed with registration
                         auth.createUserWithEmailAndPassword(enteredUsername, enteredPassword)
                             .addOnCompleteListener(this) { task ->
                                 if (task.isSuccessful) {
-                                    auth.currentUser?.sendEmailVerification()
-                                        ?.addOnCompleteListener { emailVerificationTask ->
-                                            if (emailVerificationTask.isSuccessful) {
-                                                // Email verification sent, show success message
-                                                Toast.makeText(
-                                                    this,
-                                                    "Registration successful. Please verify your email.",
-                                                    Toast.LENGTH_SHORT
-                                                ).show()
-                                            } else {
-                                                // Email verification sending failed
-                                                Toast.makeText(
-                                                    this,
-                                                    "Failed to send verification email.",
-                                                    Toast.LENGTH_SHORT
-                                                ).show()
-                                            }
+                                    auth.currentUser?.sendEmailVerification()?.addOnCompleteListener { emailVerificationTask ->
+                                        if (emailVerificationTask.isSuccessful) {
+                                            registerDialog()
+                                        } else {
+                                            Toast.makeText(
+                                                this,
+                                                "Failed to send verification email.",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
                                         }
+                                    }
                                 } else {
-                                    // Registration failed, display the error message from Firebase
                                     Toast.makeText(
                                         this,
                                         "Registration failed: ${task.exception?.message}",
@@ -76,7 +62,6 @@ class MainDisplayActivity : AppCompatActivity() {
                             }
                     }
                 } else {
-                    // Error checking user existence
                     Toast.makeText(
                         this,
                         "Error checking user existence: ${fetchTask.exception?.message}",
@@ -84,13 +69,6 @@ class MainDisplayActivity : AppCompatActivity() {
                     ).show()
                 }
             }
-        }
-
-
-        returnButton.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
         }
     }
 
@@ -106,4 +84,16 @@ class MainDisplayActivity : AppCompatActivity() {
         dialog.show()
     }
 
+    private fun registerDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Registration")
+        builder.setMessage("Verify your email!")
+        builder.setPositiveButton("Visit Dashboard") { dialog, which ->
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
 }
